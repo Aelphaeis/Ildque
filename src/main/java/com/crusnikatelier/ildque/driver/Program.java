9a90573a6922586ab4efe3a69f424dec9aa3279a
Joseph Morain <aelphaeis@live.com>
2016-09-21 17:02:53 -0400

    Modify Log4j configuration

--- src/main/java/com/crusnikatelier/ildque/driver/Program.java
+++ src/main/java/com/crusnikatelier/ildque/driver/Program.java
@@ -1,18 +1,25 @@
 package com.crusnikatelier.ildque.driver;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 import sx.blah.discord.api.ClientBuilder;
 import sx.blah.discord.api.IDiscordClient;
+import sx.blah.discord.api.IListener;
+import sx.blah.discord.handle.impl.events.MessageReceivedEvent;
+import sx.blah.discord.handle.obj.IUser;
 
 public class Program {
-	
+	private static final Logger logger = LoggerFactory.getLogger(Program.class);
 	public static void main(String[] args) throws Throwable {
-		if(args.length > 0){
-			IDiscordClient client = new ClientBuilder().withToken(args[0]).login();
-		}
-		
+		logger.info("Start Ildque");
+		IDiscordClient client =  new ClientBuilder().withToken(args[0]).login();
+		client.getDispatcher().registerListener(new IListener<MessageReceivedEvent>() {
+
+			@Override
+			public void handle(MessageReceivedEvent event) {
+				logger.info(event.getMessage().getContent());
+			}
+		});
+		IUser user = client.getUserByID("195242806795698176");
 	}
 }
