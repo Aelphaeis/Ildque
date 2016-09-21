3c622d4b524b719cfc0654894defcf11c0fe8d5e
Joseph Morain <aelphaeis@live.com>
2016-09-21 17:16:00 -0400

    added bot configuration

---
+++ src/main/java/com/crusnikatelier/ildque/BotConfiguration.java
@@ -0,0 +1,24 @@
+package com.crusnikatelier.ildque;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class BotConfiguration {
+	
+	private String commandPrefix;
+	
+	public BotConfiguration() {
+		//Default bot configuration
+		setCommandPrefix("Ilqdue");
+	}
+
+	@XmlElement
+	public String getCommandPrefix() {
+		return commandPrefix;
+	}
+
+	public void setCommandPrefix(String commandPrefix) {
+		this.commandPrefix = commandPrefix;
+	}
+}
