d03417bbcb84af6b4e88db5a1c0d8bf4c58dd338
Joseph Morain <aelphaeis@live.com>
2016-09-22 12:09:21 -0400

    Removed unnecessary code from driver.

--- src/main/java/com/crusnikatelier/ildque/driver/Program.java
+++ src/main/java/com/crusnikatelier/ildque/driver/Program.java
@@ -3,9 +3,7 @@ package com.crusnikatelier.ildque.driver;
 import com.crusnikatelier.ildque.Bot;
 
 public class Program {
-	//private static final Logger logger = LoggerFactory.getLogger(Program.class);
 	public static void main(String[] args) throws Throwable {
-		Bot bot = new Bot(args);
-		bot.run();
+		new Bot(args).run();
 	}
 }
