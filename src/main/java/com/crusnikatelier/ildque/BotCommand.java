210cdfbcc55346f77888a0f0399490ce0c9b5d48
Joseph <aelphaeis@live.com>
2016-12-12 16:52:10 -0500

    removed unused import in bot command.

--- src/main/java/com/crusnikatelier/ildque/BotCommand.java
+++ src/main/java/com/crusnikatelier/ildque/BotCommand.java
@@ -2,7 +2,6 @@ package com.crusnikatelier.ildque;
 
 import org.apache.commons.cli.OptionGroup;
 
-import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.api.events.Event;
 
 public interface BotCommand {
