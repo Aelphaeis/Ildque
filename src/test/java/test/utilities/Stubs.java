8acb9e2562bc5bb54e7d9b7de9116eecf7c77939
Joseph Morain <aelphaeis@live.com>
2016-12-13 13:59:21 -0500

    Fixed infinite loop and made stubs ctor private

--- src/test/java/test/utilities/Stubs.java
+++ src/test/java/test/utilities/Stubs.java
@@ -3,9 +3,12 @@ package test.utilities;
 import sx.blah.discord.handle.obj.IMessage;
 
 public class Stubs {
+	
 	public static final String NOT_IMPLEMENTED = "Operation has not been implemented";
 	
-	public IMessage StubMessage(){
-		return StubMessage();
+	Stubs(){ }
+
+	public static IMessage StubMessage(){
+		return new StubMessage();
 	}
 }
