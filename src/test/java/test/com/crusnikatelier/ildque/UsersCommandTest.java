2e00e26fae910c5e613ec96a648e8ba27b8e9c1f
Joseph Morain <aelphaeis@live.com>
2016-12-22 16:19:24 -0500

    Modified User Command Test

--- src/test/java/test/com/crusnikatelier/ildque/UsersCommandTest.java
+++ src/test/java/test/com/crusnikatelier/ildque/UsersCommandTest.java
@@ -2,13 +2,20 @@ package test.com.crusnikatelier.ildque;
 
 import static org.junit.Assert.*;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@SuppressWarnings("unused")
 public class UsersCommandTest {
+	
+	
+	private static final Logger logger = LoggerFactory.getLogger(UsersCommandTest.class);
 
 	@Test
 	public void test() {
@@ -16,9 +23,14 @@ public class UsersCommandTest {
 		Option o1 = new Option("o", "offline", true, "Flag for offline users");
 		group.addOption(o1);
 		
+		StringWriter sWriter = new StringWriter(5000);
+		PrintWriter pWriter = new PrintWriter(sWriter);
 		
 		HelpFormatter formatter = new HelpFormatter();
-		formatter.printHelp("help", group);	
+		formatter.printUsage(pWriter, 100, "help", group);
+		
+		assertEquals("usage: help [-o <arg>]", sWriter.toString().trim());
+		logger.info(sWriter.toString());
 	}
 
 }
