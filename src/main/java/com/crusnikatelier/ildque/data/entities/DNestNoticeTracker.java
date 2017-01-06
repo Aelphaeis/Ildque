a5b9759fa4cc20c19d6580fb9aec1478eaccd4a8
Joseph Morain <aelphaeis@live.com>
2017-01-06 16:21:15 -0500

    added annotations to DNestNoticeTracker

--- src/main/java/com/crusnikatelier/ildque/data/entities/DNestNoticeTracker.java
+++ src/main/java/com/crusnikatelier/ildque/data/entities/DNestNoticeTracker.java
@@ -2,10 +2,26 @@ package com.crusnikatelier.ildque.data.entities;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.TableGenerator;
+
+@Entity
+@Table(name="dnestnotice_tracker")
 public class DNestNoticeTracker {
-	
+	@Id
+	@Column(name="id")
+	@GeneratedValue(generator="sqlite")
+	@TableGenerator(name="sqlite", table="sqlite_sequence", pkColumnName="name", valueColumnName="seq", pkColumnValue="sqliteTestTable")
 	private long id;
+	
+	@Column(name="notice_number")
 	private long noticeNumber;
+	
+	@Column(name="notification_date")
 	private Date NotificationDate;
 	
 	public long getId() {
