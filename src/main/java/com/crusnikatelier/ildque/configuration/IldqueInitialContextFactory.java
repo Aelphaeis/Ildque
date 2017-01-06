f33c14b4b668914df32a2800b7c11211f586b3ae
Joseph <aelphaeis@live.com>
2017-01-06 18:59:15 -0500

    Added datasource to factory

--- src/main/java/com/crusnikatelier/ildque/configuration/IldqueInitialContextFactory.java
+++ src/main/java/com/crusnikatelier/ildque/configuration/IldqueInitialContextFactory.java
@@ -6,9 +6,11 @@ import java.util.Hashtable;
 import javax.naming.Context;
 import javax.naming.NamingException;
 import javax.naming.spi.InitialContextFactory;
+import javax.sql.DataSource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.sqlite.SQLiteDataSource;
 
 public class IldqueInitialContextFactory implements InitialContextFactory {
 	
@@ -25,6 +27,7 @@ public class IldqueInitialContextFactory implements InitialContextFactory {
 		//Stub in some default configuration settings
 		init.bind(Settings.PREFIX.getName(), "Ildque ");
 		init.bind(Settings.DB_CONN_STRING.getName(), getSqlite3DbConnectionString());
+		init.bind(Settings.DB_DATASOURCE.getName(), createSqlite3DbDatasource());
 		return init;
 	}
 	
@@ -32,6 +35,19 @@ public class IldqueInitialContextFactory implements InitialContextFactory {
 		String format = "jdbc:sqlite3:%s";
 		String path = new File("db/ildque.db").getAbsolutePath();
 		return String.format(format, path);
+	}
+	
+	public DataSource createSqlite3DbDatasource(){
+		try{
+			Class.forName("org.sqlite.JDBC");
+			SQLiteDataSource dataSource = new SQLiteDataSource();
+			dataSource.setUrl(getSqlite3DbConnectionString());
+			return dataSource;
+		}
+		catch (ClassNotFoundException e) {
+			logger.error(e.getMessage(), e);
+			throw new RuntimeException(e);
+		}
 	}
 	
 	
