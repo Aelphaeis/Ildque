8b5160cba9012b783b6c3702313dac7a4e9ac438
Joseph Morain <aelphaeis@live.com>
2017-01-06 15:37:05 -0500

    Added entities needed for dnestnotice command

---
+++ src/main/java/com/crusnikatelier/ildque/data/entities/DNestNoticeSubscriber.java
@@ -0,0 +1,40 @@
+package com.crusnikatelier.ildque.data.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.TableGenerator;
+
+@Entity
+@Table(name="dnestnotice_subscribers")
+public class DNestNoticeSubscriber {
+
+	@Id
+	@Column(name="id")
+	@GeneratedValue(generator="sqlite")
+	@TableGenerator(name="sqlite", table="sqlite_sequence", pkColumnName="name", valueColumnName="seq", pkColumnValue="sqliteTestTable")
+	private long id;
+	
+	@Column(name="subscriber", nullable=false, unique=true)
+	private long subscriber;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getSubscriber() {
+		return subscriber;
+	}
+
+	public void setSubscriber(long subscriber) {
+		this.subscriber = subscriber;
+	}
+	
+	
+}
---
+++ src/main/java/com/crusnikatelier/ildque/data/entities/User.java
@@ -0,0 +1,36 @@
+package com.crusnikatelier.ildque.data.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.TableGenerator;
+
+@Entity
+@Table(name="users")
+public class User {
+	
+	@Id
+	@Column(name="id")
+	@GeneratedValue(generator="sqlite")
+	@TableGenerator(name="sqlite", table="sqlite_sequence", pkColumnName="name", valueColumnName="seq", pkColumnValue="sqliteTestTable")
+	private long id;
+	
+	@Column(name="discord_id", length=40, nullable = false, unique = true)
+	private String discordId;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getDiscordId() {
+		return discordId;
+	}
+	public void setDiscordId(String discordId) {
+		this.discordId = discordId;
+	}
+
+}
