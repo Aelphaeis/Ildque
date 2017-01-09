45b6ca4e51240ce0aa2cdef98a0a85562fa833b1
Joseph Morain <aelphaeis@live.com>
2017-01-09 13:56:12 -0500

    Implemented constructors for userDAO

--- src/main/java/com/crusnikatelier/ildque/data/sqlite/impl/UserDAOImpl.java
+++ src/main/java/com/crusnikatelier/ildque/data/sqlite/impl/UserDAOImpl.java
@@ -2,6 +2,7 @@ package com.crusnikatelier.ildque.data.sqlite.impl;
 
 import java.util.List;
 
+import org.hibernate.Session;
 import org.hibernate.query.Query;
 
 import com.crusnikatelier.ildque.data.daos.UserDAO;
@@ -9,7 +10,15 @@ import com.crusnikatelier.ildque.data.entities.User;
 import com.crusnikatelier.ildque.data.sqlite.SqliteDAOBase;
 
 public class UserDAOImpl extends SqliteDAOBase implements UserDAO {
+	
+	public UserDAOImpl() {
+		super();
+	}
 
+	public UserDAOImpl(Session session) {
+		super(session);
+	}
+	
 	@Override
 	public User find(long id) {
 		String q = "select u from users u  where u.id = :id";
