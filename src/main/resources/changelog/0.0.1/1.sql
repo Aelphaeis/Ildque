13d65202a274db4c90b3e67b07e4410f3e6c0555
Joseph Morain <aelphaeis@live.com>
2017-01-09 13:56:31 -0500

    Changed SQL for users table.

--- src/main/resources/changelog/0.0.1/1.sql
+++ src/main/resources/changelog/0.0.1/1.sql
@@ -1,4 +1,6 @@
 CREATE TABLE users (
-    id        INTEGER   PRIMARY KEY,
+    id         INTEGER   PRIMARY KEY ON CONFLICT ROLLBACK AUTOINCREMENT
+                         UNIQUE
+                         NOT NULL,
     discord_id TEXT (40) UNIQUE
 );
