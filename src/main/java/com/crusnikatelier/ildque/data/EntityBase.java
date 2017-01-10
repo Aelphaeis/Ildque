6d865003c9c975bb85dcbf60e63c52121ae920d2
Joseph <aelphaeis@live.com>
2017-01-10 16:11:28 -0500

    Added logging for when entity base toString has difficulty.

--- src/main/java/com/crusnikatelier/ildque/data/EntityBase.java
+++ src/main/java/com/crusnikatelier/ildque/data/EntityBase.java
@@ -1,8 +1,13 @@
 package com.crusnikatelier.ildque.data;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.crusnikatelier.utilities.MarshalHelper;
 
 public class EntityBase {
+	
+	private static final Logger logger = LoggerFactory.getLogger(EntityBase.class);
 	
 	@Override
 	public String toString(){
@@ -10,6 +15,7 @@ public class EntityBase {
 			return MarshalHelper.marshallJson(this);
 		}
 		catch(Exception e){
+			logger.warn("Unable to marshal Object", e);
 			return super.toString();
 		}
 	}
