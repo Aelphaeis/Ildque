a0ae681b7f011a70de3cbdad18862dc23f53b259
Joseph Morain <aelphaeis@live.com>
2017-02-28 12:29:02 -0500

    added join command skeleton

---
+++ src/main/java/com/crusnikatelier/ildque/commands/JoinCommand.java
@@ -0,0 +1,32 @@
+package com.crusnikatelier.ildque.commands;
+
+import org.apache.commons.cli.Options;
+
+import com.crusnikatelier.ildque.BotCommand;
+
+import sx.blah.discord.api.events.Event;
+
+public class JoinCommand implements BotCommand {
+
+	Options options;
+	
+	public JoinCommand(){
+		options = new Options();
+	}
+	@Override
+	public String getName() {
+		return "Join";
+	}
+
+	@Override
+	public Options getOptions() {
+		return options;
+	}
+
+	@Override
+	public void execute(Event event, String[] argv) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
