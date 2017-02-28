66492f70da54f105a41e89cf9bef83d1e577cf15
Joseph Morain <aelphaeis@live.com>
2017-02-28 12:57:41 -0500

    added leave command.

---
+++ src/main/java/com/crusnikatelier/ildque/commands/LeaveCommand.java
@@ -0,0 +1,37 @@
+package com.crusnikatelier.ildque.commands;
+
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+
+import com.crusnikatelier.ildque.BotCommand;
+
+import sx.blah.discord.api.events.Event;
+import sx.blah.discord.handle.obj.IVoiceChannel;
+
+public class LeaveCommand implements BotCommand{
+
+	Options opts;
+	
+	public LeaveCommand(){
+		opts = new Options();
+		opts.addOption(new Option("", "Forceds bot to leave all connected voice channels"));
+	}
+	
+	@Override
+	public String getName() {
+		return "leave";
+	}
+
+	@Override
+	public Options getOptions() {
+		return opts;
+	}
+
+	@Override
+	public void execute(Event event, String[] argv) {
+		for(IVoiceChannel channel : event.getClient().getConnectedVoiceChannels()){
+			channel.leave();
+		}
+	}
+
+}
