efafaa8632a8a14b4a961c6336838af9b1ea8239
Joseph Morain <aelphaeis@live.com>
2017-02-28 12:07:42 -0500

    https://github.com/Aelphaeis/Ildque/issues/1 closed.

--- src/main/java/com/crusnikatelier/ildque/driver/Program.java
+++ src/main/java/com/crusnikatelier/ildque/driver/Program.java
@@ -1,5 +1,8 @@
 package com.crusnikatelier.ildque.driver;
 
+import java.io.File;
+
+import org.apache.logging.log4j.core.config.Configurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,9 +10,18 @@ import com.crusnikatelier.ildque.Bot;
 import com.crusnikatelier.ildque.configuration.BotConfiguration;
 
 public class Program {
+	static {
+		//Override the log4j2 settings
+		StringBuilder builder = new StringBuilder(System.getProperty("user.dir"));
+		builder.append("/log4j2.xml");
+		File confOverride = new File(builder.toString());
+		if(confOverride.exists() && confOverride.isFile()){
+			Configurator.initialize(null, confOverride.getAbsolutePath());
+		}
+	}
+	
 	private static final Logger logger = LoggerFactory.getLogger(Program.class);
 	public static void main(String[] args) throws Throwable {
-		
 		long start = System.currentTimeMillis();
 		new Bot(args).run();
 		long duration  = System.currentTimeMillis() - start;
