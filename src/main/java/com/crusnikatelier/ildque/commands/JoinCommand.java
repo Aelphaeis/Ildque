d0a75137fc33d56cc057229f82c54452223f2729
Joseph Morain <aelphaeis@live.com>
2017-02-28 12:57:24 -0500

    Errors now reported to user in join command, added help description.

--- src/main/java/com/crusnikatelier/ildque/commands/JoinCommand.java
+++ src/main/java/com/crusnikatelier/ildque/commands/JoinCommand.java
@@ -2,22 +2,30 @@ package com.crusnikatelier.ildque.commands;
 
 import java.util.List;
 
+import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.crusnikatelier.ildque.BotCommand;
 
 import sx.blah.discord.api.events.Event;
 import sx.blah.discord.handle.impl.events.MessageReceivedEvent;
+import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.handle.obj.IUser;
 import sx.blah.discord.handle.obj.IVoiceChannel;
+import sx.blah.discord.util.DiscordException;
 import sx.blah.discord.util.MissingPermissionsException;
+import sx.blah.discord.util.RateLimitException;
 
 public class JoinCommand implements BotCommand {
 
+	private static final Logger logger = LoggerFactory.getLogger(JoinCommand.class);
 	Options options;
 	
 	public JoinCommand(){
 		options = new Options();
+		options.addOption(new Option("", "Joins the channel the requester is in"));
 	}
 	
 	@Override
@@ -36,6 +44,22 @@ public class JoinCommand implements BotCommand {
 		MessageReceivedEvent evt =(MessageReceivedEvent)event;
 		IUser sender = evt.getMessage().getAuthor();
 		List<IVoiceChannel> channels = sender.getConnectedVoiceChannels();
+
+		try{
+			handleRequest(channels);
+		}
+		catch(IllegalStateException e){
+			try {
+				IChannel chan = evt.getMessage().getChannel();
+				chan.sendMessage(e.getMessage());
+			} 
+			catch (MissingPermissionsException | RateLimitException | DiscordException ex) {
+				logger.error("Unable to send error message to user", e);
+			}
+		}
+	}
+	
+	void handleRequest(List<IVoiceChannel> channels){
 		
 		if(channels.isEmpty()){
 			String msg = "Unable to find requester in voice channel";
