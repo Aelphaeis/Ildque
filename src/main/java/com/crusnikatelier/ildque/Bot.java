f0b2911f9dbd93b29fe6bf5430fac9dbdc695869
Joseph Morain <aelphaeis@live.com>
2017-03-28 13:31:28 -0400

    Changed bot constructor to use bot configuration

--- src/main/java/com/crusnikatelier/ildque/Bot.java
+++ src/main/java/com/crusnikatelier/ildque/Bot.java
@@ -4,6 +4,8 @@ package com.crusnikatelier.ildque;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.crusnikatelier.ildque.configuration.BotConfiguration;
+
 import sx.blah.discord.Discord4J;
 import sx.blah.discord.api.ClientBuilder;
 import sx.blah.discord.api.IDiscordClient;
@@ -17,20 +19,14 @@ public class Bot implements Runnable {
 	private static final Logger logger = LoggerFactory.getLogger(Bot.class);
 	private IDiscordClient serviceClient;
 	
-	public Bot(String []  args) throws DiscordException{
+	public Bot() throws DiscordException{
 		ClientBuilder builder = new ClientBuilder();
-		
-		if(args.length < 1){
-			throw new IllegalArgumentException("No Token Specified");
-		}
-		
 		Discord4J.ignoreChannelWarnings.set(true);
+
+		String token = BotConfiguration.value(BotConfiguration.Settings.TOKEN);
+		builder.withToken(token);
 		
-		
-		builder.withToken(args[0]);
 		serviceClient = builder.build();
-		
-		
 		
 		IListener<MessageReceivedEvent> cmdHandler = new BotCommandTextHandler(this);
 		serviceClient.getDispatcher().registerListener(cmdHandler);
