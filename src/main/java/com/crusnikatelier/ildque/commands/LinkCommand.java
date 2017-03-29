b199a900cdbd5ff8c9a1a3ee55c55cd10661c1c5
Joseph <aelphaeis@live.com>
2017-03-29 10:51:13 -0400

    refactored link command to use discordhelper and ildque exception

--- src/main/java/com/crusnikatelier/ildque/commands/LinkCommand.java
+++ src/main/java/com/crusnikatelier/ildque/commands/LinkCommand.java
@@ -5,13 +5,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.crusnikatelier.ildque.BotCommand;
+import com.crusnikatelier.ildque.exceptions.IldqueException;
+import com.crusnikatelier.utilities.DiscordHelper;
 
 import sx.blah.discord.api.events.Event;
 import sx.blah.discord.handle.impl.events.MessageReceivedEvent;
-import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.util.DiscordException;
-import sx.blah.discord.util.MissingPermissionsException;
-import sx.blah.discord.util.RateLimitException;
 
 public class LinkCommand implements BotCommand {
 	
@@ -36,21 +35,20 @@ public class LinkCommand implements BotCommand {
 	}
 
 	@Override
-	public void execute(Event event, String[] argv) {
-		try {	
+	public void execute(Event event, String[] argv) throws IldqueException {
+		try{
 			StringBuffer buffer = new StringBuffer(BASE_URL);
 			buffer.append("?client_id=");
 			buffer.append(event.getClient().getApplicationClientID());
 			buffer.append("&scope=bot");
 			
 			MessageReceivedEvent mrEvt = (MessageReceivedEvent)event;
-			IChannel chan = mrEvt.getMessage().getChannel();
-			chan.sendMessage(buffer.toString());
-			
+			DiscordHelper.sendMessage(mrEvt, buffer.toString());
 		}
-		catch (DiscordException | MissingPermissionsException | RateLimitException e) {
-			String msg = "Unable to send authorization link";
-			logger.error(msg, e);
+		catch(DiscordException e){
+			String err = "Unable to get build url";
+			logger.error(err, e);
+			throw new IldqueException(err);
 		}
 	}
 }
