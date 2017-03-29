4cb120ebd10975960528b4b620560f16f625432e
Joseph <aelphaeis@live.com>
2017-03-29 10:25:00 -0400

    Added IldqueException

---
+++ src/main/java/com/crusnikatelier/ildque/exceptions/IldqueException.java
@@ -0,0 +1,10 @@
+package com.crusnikatelier.ildque.exceptions;
+
+public class IldqueException extends Exception{
+	private static final long serialVersionUID = 1L;
+	
+	public IldqueException(String msg){
+		super(msg);
+	}
+	
+}
