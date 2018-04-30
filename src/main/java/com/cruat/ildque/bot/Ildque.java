76a8a4b8a9b2583e9d7a11b02babdd148f6a37e6
Joseph <aelphaeis@live.com>
2018-04-30 06:26:47 -0400

    ildque modifications.

--- src/main/java/com/cruat/ildque/bot/Ildque.java
+++ src/main/java/com/cruat/ildque/bot/Ildque.java
@@ -1,5 +1,8 @@
 package com.cruat.ildque.bot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -10,18 +13,18 @@ import sx.blah.discord.api.ClientBuilder;
 import sx.blah.discord.api.IDiscordClient;
 
 public class Ildque implements AutoCloseable{
+	private static final Logger logger = LogManager.getLogger();
 	
+	final List<BotCommand> commands = new ArrayList<>();
 	IDiscordClient client;
-
-	public static final Logger logger = LogManager.getLogger();
-
+	
 	public Ildque() {
 		ClientBuilder builder = new ClientBuilder();
 		builder.withToken(Settings.LOGIN_TOKEN.value(BotToken.class).getValue());
 		client = builder.build();
 		client.login();
 	}
-
+	
 	@Override
 	public void close() {
 		client.logout();
