a5e080ea41894cb0d1330133745f18a93921cc57
Joseph <aelphaeis@live.com>
2018-04-30 02:45:43 -0400

    added class to represent bot token.

---
+++ src/main/java/com/cruat/ildque/config/BotToken.java
@@ -0,0 +1,14 @@
+package com.cruat.ildque.config;
+
+public class BotToken {
+
+	String value;
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+}
