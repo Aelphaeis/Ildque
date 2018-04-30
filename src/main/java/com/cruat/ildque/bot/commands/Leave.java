f6cdb5e0475a94f74caaa6013623c1e68a590c2b
Joseph Morain <morain@echoworx.com>
2018-04-30 11:05:24 -0400

    implement leave command

--- src/main/java/com/cruat/ildque/bot/commands/Leave.java
+++ src/main/java/com/cruat/ildque/bot/commands/Leave.java
@@ -1,5 +1,23 @@
 package com.cruat.ildque.bot.commands;
 
-public class Leave {
+import org.apache.commons.cli.Option;
 
+import com.cruat.ildque.bot.exceptions.IldqueException;
+
+import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
+import sx.blah.discord.handle.obj.IVoiceChannel;
+
+public class Leave extends Command{
+	
+	public Leave() {
+		opts.addOption(new Option("", "Forces bot to leave all connected voice channels"));
+	}
+
+	@Override
+	public void execute(MessageReceivedEvent event, String[] argv)
+			throws IldqueException {
+		for(IVoiceChannel channel : event.getClient().getConnectedVoiceChannels()) {
+			channel.leave();
+		}
+	}
 }
