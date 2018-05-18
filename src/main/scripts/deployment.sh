07bc9856f321ecf2f04c414949678cff881978a9
Joseph <aelphaeis@live.com>
2018-05-18 13:57:06 -0400

    try to fix deployment script

--- src/main/scripts/deployment.sh
+++ src/main/scripts/deployment.sh
@@ -1,5 +1,5 @@
 cp .travis.settings.xml $HOME/.m2/settings.xml
-sed 's/${env.prefix}/'"${env.prefix}"'/g' .travis.configuration.xml
-sed 's/${env.prefix}/'"${env.token}"'/g' .travis.configuration.xml
+sed -i 's/${env.prefix}/'"${env.prefix}"'/g' .travis.configuration.xml
+sed -i 's/${env.prefix}/'"${env.token}"'/g' .travis.configuration.xml
 cp .travis.configuration.xml src/main/resources/configuration.xml
 mvn tomcat7:redeploy
