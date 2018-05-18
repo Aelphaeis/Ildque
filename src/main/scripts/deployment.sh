00aafb7dd26012a0887a2c030256a2d3d94c9ecd
Joseph <aelphaeis@live.com>
2018-05-18 14:04:13 -0400

    fix bug with token test.

--- src/main/scripts/deployment.sh
+++ src/main/scripts/deployment.sh
@@ -1,5 +1,5 @@
 cp .travis.settings.xml $HOME/.m2/settings.xml
 sed -i 's/${env.prefix}/test/g' .travis.configuration.xml
-sed -i 's/${env.prefix}/test/g' .travis.configuration.xml
+sed -i 's/${env.token}/test/g' .travis.configuration.xml
 cp .travis.configuration.xml src/main/resources/configuration.xml
 mvn tomcat7:redeploy
