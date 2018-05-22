ee9fa5e2d24fc8913337fc98338e4916a0f063f6
Joseph <aelphaeis@live.com>
2018-05-22 01:08:26 -0400

    remove cp step from deployment script

--- src/main/scripts/deployment.sh
+++ src/main/scripts/deployment.sh
@@ -1,5 +1,4 @@
 cp .travis.settings.xml $HOME/.m2/settings.xml
-sed -i 's/${env.prefix}/'$prefix'/g' .travis.configuration.xml
-sed -i 's/${env.token}/'$token'/g' .travis.configuration.xml
-cp .travis.configuration.xml src/main/resources/configuration.xml
+sed -i 's/${env.prefix}/'$prefix'/g'  src/main/resources/configuration.xml
+sed -i 's/${env.token}/'$token'/g'  src/main/resources/configuration.xml
 mvn tomcat7:redeploy 
