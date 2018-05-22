5c293c612f0fe56fbee8c3c286571130d87130d7
Joseph <aelphaeis@live.com>
2018-05-22 02:02:52 -0400

    command to show website link added.

---
+++ src/main/java/com/cruat/ildque/bot/commands/Site.java
@@ -0,0 +1,17 @@
+package com.cruat.ildque.bot.commands;
+
+import com.cruat.ildque.bot.exceptions.IldqueException;
+import com.cruat.ildque.bot.utilities.DiscordHelper;
+
+import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
+
+public class Site extends Command {
+
+	public static final String ILDQUE_LINK = "http://ildque.cruat.com";
+	
+	@Override
+	public void execute(MessageReceivedEvent event, String[] argv) throws IldqueException {
+		String message = "<" + ILDQUE_LINK + ">";
+		DiscordHelper.sendMessage(event, message);
+	}
+}
