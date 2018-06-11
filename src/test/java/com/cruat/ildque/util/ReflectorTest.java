8ff7ce9a836c858507140a0eef6fd33cdc9a20b0
Joseph Morain <morain@echoworx.com>
2018-06-11 16:15:28 -0400

    more tests for reflector.

--- src/test/java/com/cruat/ildque/util/ReflectorTest.java
+++ src/test/java/com/cruat/ildque/util/ReflectorTest.java
@@ -16,9 +16,52 @@ public class ReflectorTest {
 		Package pkg = Reflector.class.getPackage();
 		assertNotNull(loader);
 		assertNotNull(pkg);
-		
+
 		List<Class<?>> classes = Reflector.getClassesForPackage(pkg, loader);
 		assertTrue(classes.contains(Reflector.class));
 	}
 
+	@Test
+	public void ctor_various_success() {
+		new Reflector.ReflectorException();
+		new Reflector.ReflectorException("");
+		new Reflector.ReflectorException("", new Exception());
+		new Reflector.ReflectorException(new Exception());
+	}
+
+	@Test
+	public void isInstantiable_various_correct() {
+		String[] arr = new String[0];
+		assertFalse(Reflector.isInstantiable(null));
+		assertFalse(Reflector.isInstantiable(String.class));
+		assertFalse(Reflector.isInstantiable(Integer.class));
+		assertFalse(Reflector.isInstantiable(arr.getClass()));
+		assertFalse(Reflector.isInstantiable(TestInterface.class));
+		assertFalse(Reflector.isInstantiable(byte.class));
+		assertFalse(Reflector.isInstantiable(TestAbstract.class));
+		assertTrue(Reflector.isInstantiable(TestConcrete.class));
+		assertFalse(Reflector.isInstantiable(Reflector.class));
+		assertTrue(Reflector.isInstantiable(TestConcreteCtor.class));
+	}
+
+	@Test
+	public void initParamCtor_validClass_instance() {
+		assertNotNull(Reflector.initParamCtor(TestConcreteCtor.class));
+	}
+
+	@Test
+	public void initParamCtor_privateCtor_null() {
+		assertNull(Reflector.initParamCtor(Reflector.class));
+	}
+
+	public static interface TestInterface {
+	}
+	public static abstract class TestAbstract implements TestInterface {
+	}
+	public static class TestConcrete extends TestAbstract {
+	}
+	public static class TestConcreteCtor extends TestConcrete {
+		public TestConcreteCtor() {
+		}
+	}
 }
